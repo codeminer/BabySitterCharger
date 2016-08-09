ad231ef183538aa6aaa11dd3604f491c8b88429e
yadelani <yussuf.kazade@gmail.com>
2016-08-09 03:04:45 -0500

    first test commit

---
+++ BabySitterChargeTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabySitterCharge;
+
+namespace BabySitterChargeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestChargeOne()
+        {
+            BabySitter babySitter = new BabySitter();
+
+            Assert.AreEqual(144, babySitter.getPayCharge("5 PM", "11 PM", "4 AM"));
+        }
+    }
+}
---
+++ BabySitterCharge_Test/ChargeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabySitterCharge;
+
+namespace BabySitterCharge_Test
+{
+    [TestClass]
+    public class ChargeTest
+    {
+        [TestMethod]
+        public void TestCharge()
+        {
+            BabySitter babySitter = new BabySitter();
+
+            Assert.AreEqual(144, babySitter.getPayCharge("11 PM", "5 PM", "4 AM"));
+        }
+    }
+}
